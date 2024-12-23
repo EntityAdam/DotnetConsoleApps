afdcb2780bd52272047c15c3e294223444660f1c
Adam Vincent <entityadam@outlook.com>
2024-12-22 23:40:55 -0500

    added missing await

--- Net7WithHostBuilder/Program.cs
+++ Net7WithHostBuilder/Program.cs
@@ -7,7 +7,7 @@ var hostBuilder = Host.CreateApplicationBuilder(args);
 hostBuilder.Services.AddHostedService<DoWorkAndStopService>()
                     .AddTransient<IKeyvaultClient, KeyvaultClient>();
 
-hostBuilder.Build().RunAsync();
+await hostBuilder.Build().RunAsync();
 
 //some *light* reading: https://github.com/dotnet/runtime/issues/61634#issuecomment-1033038101
 
